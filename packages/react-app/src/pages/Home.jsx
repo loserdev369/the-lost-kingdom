f8e795f530959c67be10039663a5afad67996f7a
loserdev369 <loserdev@protonmail.com>
2021-12-04 08:09:44 -0500

    create Mint.jsx and added it to home

--- packages/react-app/src/pages/Home.jsx
+++ packages/react-app/src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import NavBar from "../components/NavBar.jsx"
+import Mint from "../components/Mint.jsx"
 import styled from '@emotion/styled'
 import TLKTitle from '../assets/pages/hero/the-lost-kingdom.png'
 import TLKIntroText from '../assets/pages/hero/once-upon-a-time.png'
 import LearnMoreBtn from '../assets/pages/hero/learn-more-btn.png'
 import TLKBG from '../assets/pages/hero/hero-bg-image.png'
 
-const BG = styled.div`
+const Hero = styled.div`
   background: url(${TLKBG}) center center ;
   background-size: cover;
   height: 100vh;
@@ -42,16 +43,19 @@ const LearnMore = styled.img`
 
 function Home() {
   return (
-    <BG>
-      <NavBar />
-      <MiddleSection>
-        <div>
-          <Title src={TLKTitle} />
-          <IntroText src={TLKIntroText} />
-          <LearnMore src={LearnMoreBtn} />
-        </div>
-      </MiddleSection>
-    </ BG>
+    <>
+      <Hero>
+        <NavBar />
+        <MiddleSection>
+          <div>
+            <Title src={TLKTitle} />
+            <IntroText src={TLKIntroText} />
+            <LearnMore src={LearnMoreBtn} />
+          </div>
+        </MiddleSection>
+      </ Hero>
+      <Mint />
+    </>
   )
 }
 
