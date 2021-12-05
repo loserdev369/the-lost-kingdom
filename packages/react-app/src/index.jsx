127319cc7b3ea2cc11d86e4172ec487884f63155
loserdev369 <loserdev@protonmail.com>
2021-12-05 09:38:57 -0500

    Added gsap to the project

--- packages/react-app/src/index.jsx
+++ packages/react-app/src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'
 import Home from './pages/Home.jsx'
 import { ChakraProvider } from "@chakra-ui/react"
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
