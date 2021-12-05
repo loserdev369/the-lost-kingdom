8ea79cf3f7e6b40f6850ead373b247400192e5b6
loserdev369 <loserdev@protonmail.com>
2021-12-05 09:38:15 -0500

    Added animations and functionality to the audioWaveBtn

--- packages/react-app/src/components/NavBar.jsx
+++ packages/react-app/src/components/NavBar.jsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useRef, useEffect, useState } from "react"
 import styled from "@emotion/styled"
 import { css } from "@emotion/react"
+import { gsap } from 'gsap'
 import home from '../assets/pages/hero/nav/home.png'
 import about from '../assets/pages/hero/nav/about.png'
 import roadmap from '../assets/pages/hero/nav/roadmap.png'
@@ -11,6 +12,8 @@ import twitter from '../assets/pages/hero/nav/twitter-icon.png'
 import discord from '../assets/pages/hero/nav/discord-icon.png'
 import music from '../assets/pages/hero/nav/music-icon.png'
 import connect from '../assets/pages/hero/nav/connect-btn.png'
+import { Howl } from 'howler'
+import spirtOfGresynu from '../assets/music/spirit-of-gresynu.mp3'
 
 const NavWrap = styled.div`
   display: flex;
@@ -51,21 +54,81 @@ const NavItem = styled.img`
 `
 
 function NavBar() {
+
+  const homeRef = useRef();
+  const aboutRef = useRef();
+  const roadmapRef = useRef();
+  const teamRef = useRef();
+  const faqRef = useRef();
+  const loreRef = useRef();
+
+  const twitterRef = useRef();
+  const discordRef = useRef();
+  const audioWaveRef = useRef();
+  const connectBtnRef = useRef();
+
+  const [currentSound, setCurrentSound] = useState()
+  const [soundID, setSoundId] = useState(0)
+  const [isStopped, setIsStopped] = useState(false)
+
+  useEffect(() => {
+    const refs = [
+      homeRef.current,
+      aboutRef.current,
+      roadmapRef.current,
+      teamRef.current,
+      faqRef.current,
+      loreRef.current
+    ]
+    const socialRefs = [
+      twitterRef.current,
+      discordRef.current,
+      audioWaveRef.current,
+      connectBtnRef.current
+    ]
+    gsap.from(refs, { y: -200, stagger: 0.3, opacity: 0 })
+    gsap.from(socialRefs, { y: -200, stagger: 0.3, opacity: 0, delay: 1 })
+  }, [])
+
+  useEffect(() => {
+    // edit volume so that its not to loud while playing
+    const themeMusic = new Howl({ src: spirtOfGresynu, loop: true, html5: true })
+    setCurrentSound(themeMusic)
+    const soundId = themeMusic.play()
+    setSoundId(soundId)
+  }, [])
+
+  const handleMusic = () => {
+    // check if the sound is playing
+    if (!isStopped) {
+      // if so pause sound
+      setIsStopped(true);
+      currentSound.fade(100, 0, 3000, soundID)
+      currentSound.pause(soundID)
+      // console.log({ currentSound, soundID }, currentSound.seek(soundID))
+    } else {
+      setIsStopped(false);
+      currentSound.fade(0, 100, 600, soundID)
+      currentSound.play(soundID)
+      // console.log({ currentSound, soundID }, currentSound.seek(soundID))
+    }
+  }
+
   return (
     <NavWrap>
       <NavBox hide>
-        <NavItem src={home} />
-        <NavItem src={about} />
-        <NavItem src={roadmap} />
-        <NavItem src={team} />
-        <NavItem src={faq} />
-        <NavItem src={lore} />
+        <NavItem ref={homeRef} src={home} />
+        <NavItem ref={aboutRef} src={about} />
+        <NavItem ref={roadmapRef} src={roadmap} />
+        <NavItem ref={teamRef} src={team} />
+        <NavItem ref={faqRef} src={faq} />
+        <NavItem ref={loreRef} src={lore} />
       </NavBox>
       <NavBox>
-        <NavItem left src={twitter} />
-        <NavItem left src={discord} />
-        <NavItem left src={music} />
-        <NavItem connect left src={connect} />
+        <NavItem ref={twitterRef} left src={twitter} />
+        <NavItem ref={discordRef} left src={discord} />
+        <NavItem onClick={handleMusic} ref={audioWaveRef} left src={music} />
+        <NavItem ref={connectBtnRef} connect left src={connect} />
       </NavBox>
     </NavWrap>
   )
