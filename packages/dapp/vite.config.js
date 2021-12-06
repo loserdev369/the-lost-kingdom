0871ed79e05edb0d671c93e6ce1184812bd6747d
loserdev369 <loserdev@protonmail.com>
2021-12-06 18:04:52 -0500

    updated vite config to have sourcemaps

--- packages/dapp/vite.config.js
+++ packages/dapp/vite.config.js
@@ -4,6 +4,9 @@ import svgrPlugin from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true
+  },
   plugins: [
     react(),
     svgrPlugin({
