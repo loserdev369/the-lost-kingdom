539f8fe9237eb97e91e0727fa28e029e7f883222
loserdev369 <loserdev@protonmail.com>
2021-12-08 12:08:51 -0500

    Added custom fonts and refactored Home.jsx

--- packages/dapp/src/pages/Home.jsx
+++ packages/dapp/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Mint from "../components/Mint.jsx"
 import styled from '@emotion/styled'
-import { Global, css } from "@emotion/react"
 import bgTexture from '../assets/pages/mint/bg-texture.png'
 import Hero from '../components/Hero.jsx'
 import About from '../components/About.jsx'
@@ -10,6 +9,7 @@ import Team from '../components/Team.jsx'
 import FAQ from '../components/Faq.jsx'
 import Footer from '../components/Footer.jsx'
 import { Provider } from '../context/Provider.jsx'
+import GlobalStyles from '../components/Providers/Global.jsx'
 
 const HomeWrap = styled.div`
   background: url(${bgTexture});
@@ -21,25 +21,16 @@ function Home() {
 
   return (
     <HomeWrap>
-      <Global
-        styles={css`
-          body {
-            background: linear-gradient(180deg, rgba(34,63,43,1) 0%, rgba(73,161,132,1) 50%, rgba(34,63,43,1) 100%);
-            position: relative;
-          }
-          img {
-            margin: 0 auto;
-          }
-        `}
-      />
       <Provider>
-        <Hero />
-        <Mint />
-        <About />
-        <Roadmap />
-        <Team />
-        <FAQ />
-        <Footer />
+        <GlobalStyles>
+          <Hero />
+          <Mint />
+          <About />
+          <Roadmap />
+          <Team />
+          <FAQ />
+          <Footer />
+        </GlobalStyles>
       </Provider>
     </HomeWrap>
   )
